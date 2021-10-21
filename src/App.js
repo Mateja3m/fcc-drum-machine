b330e1ace270b75641b0c776941ffc83f611f8ab
Milan <milanmatejicdev@gmail.comn>
2021-10-21 22:20:40 +0200

    created functional react app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import Music from './Music';
-import {audioList} from './data'
-
-
+import Music from "./Music";
+import { audioList } from "./data";
+import {useState} from 'react'
 
 function App() {
+  const [volume, setVolume] = useState('0.6');
+  const [record, setRecord]= useState('');
   return (
     <div className="bg-light min-vh-100 text-success justify-content">
-  <div className='text-center'>
-  <h2>Drum Machine</h2>
-  {audioList.map((clip)=> 
-  (<Music key={clip.id} clip={clip} />)
-  )}
-  </div>
+      <div className="text-center">
+        <h2>Drum Machine</h2>
+        {audioList.map((clip) => (
+          <Music key={clip.id} clip={clip} volume={volume} setRecord={setRecord} />
+        ))}
+        <br />
+        <label className="volume-name">Volume</label> <br/>
+        <input className="w-40" type='range' step='0.01' name='volume' min='0' max='1' value={volume} onChange={(e)=> setVolume(e.target.value)} />
+        <h4 id='display'>{record}</h4>
+      </div> 
     </div>
   );
 }
--- src/Music.js
+++ src/Music.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 
-const Music = ({clip}) => { 
-    useEffect(()=> {
-        document.addEventListener('keydown', handleKeyPress)
-        return ()=> {document.removeEventListener('keydown', handleKeyPress)}
-    })
+const Music = ({ clip, volume, setRecord }) => {
+  const [active, setActive] = useState(false);
+  
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
 
-    const handleKeyPress = (e) => {
-        e.preventDefault();
-        if(e.keyCode === clip.keyCode) {
-            playMusic()
-        }
+  const handleKeyPress = (e) => {
+    e.preventDefault();
+    if (e.keyCode === clip.keyCode) {
+      playMusic();
     }
+  };
 
-    function playMusic() {
-        const audioTag = document.getElementById(clip.keyTrigger);
-        audioTag.currentTime = 0;
-        audioTag.play();
-    }
+  function playMusic() {
+    const audioTag = document.getElementById(clip.keyTrigger);
+    audioTag.currentTime = 0;
+    audioTag.volume = volume;
+    setTimeout(() => {
+      setActive(false);
+      setRecord('');
+    }, 800);
+    setActive(true);
+    audioTag.play();
+    setRecord((prev)=> prev + clip.id + " ")
+  }
 
-    return (
-    <button onClick={playMusic} className={`btn btn-success p-3 m-2`} id="clip.keyTrigger">
-        <audio id={clip.keyTrigger} src={clip.url}/>{clip.keyTrigger}
+  return (
+    <button
+      onClick={playMusic}
+      className={`btn btn-success p-3 m-2 ${active && "btn-danger"}`}
+      id="clip.keyTrigger"
+    >
+      <audio id={clip.keyTrigger} src={clip.url} />
+      {clip.keyTrigger}
     </button>
-    )
-    }
+  );
+};
 
-export default Music
+export default Music;
