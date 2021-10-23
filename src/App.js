629744555b357239ac98c55ae236369ff6851d39
Milan <milanmatejicdev@gmail.comn>
2021-10-23 20:22:46 +0200

    added grid style to the app

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
   const [record, setRecord]= useState('');
   return (
     <div className="bg-light min-vh-100 text-success justify-content">
-      <div className="text-center">
         <h2>Drum Machine</h2>
+      <div className="text-center wrapper">
         {audioList.map((clip) => (
           <Music key={clip.id} clip={clip} volume={volume} setRecord={setRecord} />
         ))}
         <br />
         <label className="volume-name">Volume</label> <br/>
-        <input className="w-40" type='range' step='0.01' name='volume' min='0' max='1' value={volume} onChange={(e)=> setVolume(e.target.value)} />
+        <input className="w-40 volume-slider" type='range' step='0.01' name='volume' min='0' max='1' value={volume} onChange={(e)=> setVolume(e.target.value)} /> 
         <h4 id='display'>{record}</h4>
       </div> 
     </div>
